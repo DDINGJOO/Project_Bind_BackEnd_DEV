8e6dd9218c41568a7ec97fcc2c04ba5c765a1d16
DDINGJOO <ddingsha9@teambind.co.kr>
2025-05-29 14:29:25 +0900

    FEAT : USERINFO INIT

---
+++ service/userInfo/src/main/java/bind/userInfo/UserInfoApplication.java
@@ -0,0 +1,12 @@
+package bind.userInfo;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInfoApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(UserInfoApplication.class, args);
+    }
+}
